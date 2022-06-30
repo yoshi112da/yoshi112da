24462dce32ff5c349e073ca3379d54b0a8eb6acd
yoshi112da <105767879+yoshi112da@users.noreply.github.com>
2022-06-30 22:14:44 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+/* const it=document.getElementById("item");
+const ti=document.getElementById("time");
+ 
+it.addEventListener("click",function(){
+  window.location.href = '/html/item.html'
+});
+ti.addEventListener("click",function(){
+  window.location.href="{/html/time.html}"
+});
